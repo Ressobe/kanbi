c69c1e5065ca3e9197355228b17a01d5fa40effa
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-03-02 20:17:09 +0100

    feat: add sign-in and sign-up forms

---
+++ src/components/forms/sign-in-form.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { SignInFormType, signInSchema } from "@/app/types/formTypes";
+import { useRouter } from "next/navigation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { signIn } from "next-auth/react";
+import { useToast } from "../ui/use-toast";
+
+type SignInFormProps = {
+  onSuccess: () => void;
+};
+
+export function SignInForm({ onSuccess }: SignInFormProps) {
+  const form = useForm<SignInFormType>({
+    resolver: zodResolver(signInSchema),
+  });
+  const { toast } = useToast();
+
+  const router = useRouter();
+
+  const onSubmit = async (values: SignInFormType) => {
+    const signInData = await signIn("credentials", {
+      email: values.email,
+      password: values.password,
+      redirect: false,
+    });
+
+    if (signInData?.error) {
+      console.error(signInData.error);
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Invalid password or email",
+      });
+    } else {
+      onSuccess();
+      router.refresh();
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        method="post"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 flex flex-col p-6"
+      >
+        <FormField
+          control={form.control}
+          defaultValue=""
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="*Your email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          defaultValue=""
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="*Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="w-full flex justify-center">
+          <Button type="submit" className="px-20 rounded">
+            Log in
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
---
+++ src/components/forms/sign-up-form.tsx
@@ -0,0 +1,147 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { SignUpFormType, signUpSchema } from "@/app/types/formTypes";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { useToast } from "../ui/use-toast";
+
+
+type SignUpFormProps = {
+  onSuccess: () => void;
+}
+
+export function SignUpForm({onSuccess}: SignUpFormProps) {
+  const { toast } = useToast();
+
+  const form = useForm<SignUpFormType>({
+    resolver: zodResolver(signUpSchema)
+  });
+
+  const onSubmit = async (values: SignUpFormType) => {
+    const response = await fetch('api/user/sign-up', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values)
+    });
+
+    const data = await response.json();
+
+    if (data.errors) {
+      const errors = data.errors;
+      if (errors.email) {
+        form.setError("email", {
+          type: 'server',
+          message: errors.email
+        });
+      }
+      if (errors.username) {
+        form.setError("username", {
+          type: 'server',
+          message: errors.username
+        });
+
+      }
+    }
+
+    if (response.ok) {
+      onSuccess();
+      toast(
+        {
+          title: "Your account was created", 
+          description: "Now log into your new account" 
+        }
+      );
+      return;
+    }
+
+    if (!data.errors) {
+      toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: "There was a problem with your request.",
+        });
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form method="post" onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex flex-col p-4">
+        <FormField
+            control={form.control}
+            name="email" 
+            defaultValue=""
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                        <Input placeholder="*Your email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          defaultValue=""
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormControl>
+                <Input placeholder="*Username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          defaultValue=""
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="*Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          defaultValue=""
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="*Confirm password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="w-full flex justify-center">
+          <Button type="submit" className="px-20 rounded">Create Account</Button>
+        </div>
+      </form>
+    </Form>
+  )
+}

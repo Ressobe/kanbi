48c148994be21eaadb501bd73e139b9b2b107088
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-03-03 21:24:32 +0100

    remove file

--- src/actions/create-task.ts
+++
@@ -1,7 +0,0 @@
-"use server"
-
-import { addTask } from "@/database/tasks";
-
-export default async function createTaskAction(columnId: string, content: string) {
-    return await addTask(columnId, content);
-}
